522878c8cb34967aa7e71f595d35f30ad472ce55
StaanERP <jegathish.e@staan.in>
2026-04-01 05:23:33 +0530

    first commit

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,36 @@
+import 'package:coffeshop/Pages/cart_page.dart';
+import 'package:coffeshop/Pages/shop_page.dart';
+import 'package:coffeshop/components/bottom_nav_.dart';
+import 'package:coffeshop/const.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    int selectedIndex = 0;
+
+    void navigateBottomBar(int index) {
+      print(index);
+      setState(() {
+        selectedIndex = index;
+      });
+    }
+
+    final List<Widget> pages = [ShopPage(), CartPage()];
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: Center(child: Text('$selectedIndex')),
+    );
+  }
+}
